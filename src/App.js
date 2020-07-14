07d6192327534fdd6cf1fd0c94d5122cec32bb96
Aman Goyal <im.developer26@gmail.com>
2020-07-14 14:50:37 +0530

    join chat component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Chat, Join } from './components'
 
 class App extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <Router>
+                <Route path='/' exact component={Join}></Route>
+                <Route path='/chat' component={Chat}></Route>
+            </Router>
         );
     }
 }
